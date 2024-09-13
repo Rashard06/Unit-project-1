afed3dc0c3e5246673bbd7cd88266cf6bcb4eda1
Rashard06 <127390@epsstudents.org>
2024-09-13 14:26:06 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,8 +2,27 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
       Scanner input = new Scanner(System.in);
-      System.out.print("Enter a noun ");
-      int noun1 = input.nextInt();
+      System.out.print("");
+      String noun1 = input.nextLine();
+      System.out.print("Enter a number ");
+      String noun2 = input.nextLine();
+      System.out.print("Enter a number ");
+      String noun3 = input.nextLine();
+      System.out.print("Enter a number ");
+      String noun4 = input.nextLine();
+      System.out.print("Enter a number ");
+      String noun5 = input.nextLine();
+      System.out.print("Enter a number ");
+      String noun6 = input.nextLine();
+      System.out.print("How many brothers do you have ");
+      int number = input.nextInt();
+      System.out.print("do you have you drivers lisence ");
+      boolean tf= input.nextBoolean();
+      System.out.print("Enter a number ");
+      int number1 = input.nextInt();
+      System.out.print("Enter a number ");
+      double number2 = input.nextDouble();
+      
 }
     
 }
