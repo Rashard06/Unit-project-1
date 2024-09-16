6438e1afd58f9a780199beefbd8b97a8272c9c74
Rashard06 <127390@epsstudents.org>
2024-09-16 17:13:35 -0400

    This is most likely the finished code

--- Main.java
+++ Main.java
@@ -9,20 +9,20 @@ public class Main{
       System.out.print("Enter a name of building ");
       String noun3 = input.nextLine();
       System.out.print("Enter a number ");
-      String noun4 = input.nextLine();
+      int homework = input.nextInt();
       System.out.print("Enter a number ");
-      String noun5 = input.nextLine();
-      System.out.print("Enter a number ");
-      String age = input.nextLine();
+      int days = input.nextInt();
+      System.out.print("Enter a number of someones age ");
+      int age = input.nextInt();
       System.out.print("How many brothers do you have ");
       int number = input.nextInt();
       System.out.print("Enter true or false ");
       boolean tf= input.nextBoolean();
-      System.out.print("Enter a number ");
+      System.out.print("Enter your age ");
       int number1 = input.nextInt();
       System.out.print("Enter a decimal number ");
       double decimal_number = input.nextDouble();
-      System.out.println("Today was the " + number + " week of school. I was doing well until my " + pet + "attacked my "+ gaurdian + " .This was a shock when I got home and gound. I had to rush to the " + noun3 + "The doctor then asked what is your " + gaurdian + "age and your age. I responded by stating my age as " + number1 + "and my" + gaurdian + " age as " + age + ". The docter then measured my " + gaurdian + "and said her weight was" + decimal_number + "The docter asked us a couple of true false questions "); 
+      System.out.println("Today was the " + number + "th week of school. I was doing well until a " + pet + " attacked my " + gaurdian + ". This was a shock when I got home and gound. I had to rush to the " + noun3 + " The doctor then asked what is your " + gaurdian + " age and your age. I responded by saying my age was " + number1 + " and my " + gaurdian + " age was " + age +". The docter then measured my " + gaurdian + " and said there weight was " + decimal_number + "lb The docter asked us a couple of true false questions. I answer " + tf + " for all the questions. After that we left and went home. " + days + " days later everyone was fine. I now have to catch up on " + homework + " missing assignments from school."); 
       
 }
     
